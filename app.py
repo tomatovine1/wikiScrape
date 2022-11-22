419598115de7ef551d9742f4b397f141c030749c
tomatovine1 <hesmaeilz01@gmail.com>
2022-11-21 23:59:10 -0500

    importing libraries, create a chrome webdriver instance

---
+++ app.py
@@ -0,0 +1,16 @@
+
+# Importing Libraries
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Options
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+# Creating WebDriver instance
+wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
