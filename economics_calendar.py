26332b78529dc057bec0ed62cb08bdfd5278bcd7
tomatovine1 <hesmaeilz01@gmail.com>
2022-11-25 21:53:00 -0500

    table elements scraped as requested, needs dataframe, processing nulls

--- economics_calendar.py
+++ economics_calendar.py
@@ -14,12 +14,19 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
 wd.get("https://tradingeconomics.com/calendar")
+wd.maximize_window()
 assert "Economic Calendar" in wd.title
 
-# Make Python sleep for some time#sleep(2)
+countries=wd.find_elements(By.XPATH,'//table[@id="calendar"]/tbody/tr/td[2]')
+event=wd.find_elements(By.XPATH,'//table[@id="calendar"]/tbody/tr/td[3]' )
+actual=wd.find_elements(By.XPATH,'//table[@id="calendar"]/tbody/tr/td[4]' )
+previous=wd.find_elements(By.XPATH,'//table[@id="calendar"]/tbody/tr/td[5]' )
+concensus=wd.find_elements(By.XPATH,'//table[@id="calendar"]/tbody/tr/td[6]' )
 
-
-time.sleep(2)
-for table in wd.find_elements('//*[contains(@id,"eventHistoryTable")]//tr'):
-    data = [item.text for item in table.find_elements(".//*[self::td or self::th]")]
-    print(data)
+for c in actual:
+   print(c.text)
+# print(str(len(countries)) + str(countries[1]))
+# print(str(len(event)) + str(event[1]))
+# print(str(len(actual)) + str(actual[1]))
+# print(str(len(previous)) + str(previous[1]))
+# print(str(len(concensus)) + str(concensus[1]))
